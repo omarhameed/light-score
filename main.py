d61ec4fc89ecba309c0248b7934bc8c6a147526a
omarhameed <omarg@dal.ca>
2024-08-16 21:48:33 -0400

    Add light score route

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from typing import Union
+import random
 
 from fastapi import FastAPI
 
@@ -13,3 +14,24 @@ def read_root():
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
+
+
+
+@app.get("/light_score/")
+def get_light_score(
+    country: str,
+    city: str,
+    postal_code: str,
+    street_number: str,
+    floor: Union[str, None] = None
+):
+    # Generate a random score between 0 and 100
+    light_score = random.randint(0, 100)
+    return {
+        "country": country,
+        "city": city,
+        "postal_code": postal_code,
+        "street_number": street_number,
+        "floor": floor,
+        "light_score": light_score
+    }
